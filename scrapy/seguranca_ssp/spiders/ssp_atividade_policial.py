61312b075193693c59d14e1e0c55b5467cdd166c
d116626 <d116626@gmail.com>
2019-10-23 18:15:32 -0300

    fast commit

--- scrapy/seguranca_ssp/spiders/ssp_atividade_policial.py
+++ scrapy/seguranca_ssp/spiders/ssp_atividade_policial.py
@@ -4,7 +4,36 @@ import json
 import pandas as pd
 import time
 import numpy as np
+import csv
+import os
 
+
+def append_to_csv(df, file,path):
+    file_pre=file+"_MODELO.csv"
+    file = path + file  +".csv"
+    
+    if not os.path.exists(file):
+
+        df_modelo = pd.read_csv(file_pre,encoding='utf-8')
+
+        df_modelo.to_csv(file, index = False, header = True, encoding='utf-8')
+
+        with open(file) as f:
+            header = next(csv.reader(f))
+        columns = df.columns
+        for column in set(header) - set(columns):
+            df[column] = ''
+        df = df[header]
+        df.to_csv(file, index = False, header = False, mode = 'a', encoding='utf-8')
+        
+    else:
+        with open(file) as f:
+            header = next(csv.reader(f))
+        columns = df.columns
+        for column in set(header) - set(columns):
+            df[column] = ''
+        df = df[header]
+        df.to_csv(file, index = False, header = False, mode = 'a', encoding='utf-8')
 
 class IcmsSpider(scrapy.Spider):
     name = 'icms'
@@ -19,7 +48,7 @@ class IcmsSpider(scrapy.Spider):
         municipipos =[str(i) for i in range(1,646)]
         delegacia = '0'
         #iterate over all cities and years
-        i=0
+
         for municipipo in municipipos:
             for year in years:
                 EVENTVALIDATION = response.xpath("//*[@id='__EVENTVALIDATION']/@value").extract_first()
@@ -38,15 +67,6 @@ class IcmsSpider(scrapy.Spider):
                 header = {
                     'Content-Type':' application/x-www-form-urlencoded'
                 }
-                
-
-                if i ==0:
-                    df = pd.DataFrame(columns=[i for i in range(1,31)])
-                    df.to_csv('ssp_atividade_policial.csv',mode='a', header=True ,index=False, encoding='utf-8')
-                i+=1
-                
-
-
                 yield scrapy.FormRequest(self.site_url, headers=header, formdata = data, callback=self.parse_months,  dont_filter=False)
 
     def parse_months(self,response):
@@ -94,7 +114,7 @@ class IcmsSpider(scrapy.Spider):
         final_cols = ['municipio','regiao','delegacia','ano'] + cols_name + ['data_de_captura']
         df = df[final_cols]
 
-        # print(df)
-        df.to_csv('ssp_atividade_policial.csv',mode='a', header=True ,index=False, encoding='utf-8')
+        path = '../../../dados/'
+        append_to_csv(df,'ssp_atividade_policial',path)
 
         # time.sleep(10)
--- scrapy/seguranca_ssp/spiders/ssp_ocorrencias.py
+++ scrapy/seguranca_ssp/spiders/ssp_ocorrencias.py
@@ -4,6 +4,35 @@ import json
 import pandas as pd
 import time
 import numpy as np
+import os
+import csv
+
+def append_to_csv(df, file,path):
+    file_pre=file+"_MODELO.csv"
+    file = path + file  +".csv"
+    
+    if not os.path.exists(file):
+
+        df_modelo = pd.read_csv(file_pre,encoding='utf-8')
+
+        df_modelo.to_csv(file, index = False, header = True, encoding='utf-8')
+
+        with open(file) as f:
+            header = next(csv.reader(f))
+        columns = df.columns
+        for column in set(header) - set(columns):
+            df[column] = ''
+        df = df[header]
+        df.to_csv(file, index = False, header = False, mode = 'a', encoding='utf-8')
+        
+    else:
+        with open(file) as f:
+            header = next(csv.reader(f))
+        columns = df.columns
+        for column in set(header) - set(columns):
+            df[column] = ''
+        df = df[header]
+        df.to_csv(file, index = False, header = False, mode = 'a', encoding='utf-8')
 
 
 class IcmsSpider(scrapy.Spider):
@@ -12,6 +41,7 @@ class IcmsSpider(scrapy.Spider):
     # allowed_domains = ['www.fazenda.sp.gov.br/RepasseConsulta/Consulta/repasse.aspx']
     start_urls = [site_url]
 
+
     def parse(self, response):
         #list of all cities codes
         years = [str(i) for i in range(2002,2020)]
@@ -19,7 +49,6 @@ class IcmsSpider(scrapy.Spider):
         municipipos =[str(i) for i in range(1,646)]
         delegacia = '0'
         #iterate over all cities and years
-        i=0
         for municipipo in municipipos:
             for year in years:
                 EVENTVALIDATION = response.xpath("//*[@id='__EVENTVALIDATION']/@value").extract_first()
@@ -38,10 +67,7 @@ class IcmsSpider(scrapy.Spider):
                 header = {
                     'Content-Type':' application/x-www-form-urlencoded'
                 }
-                if i ==0:
-                    df = pd.DataFrame(columns=[i for i in range(1,31)])
-                    df.to_csv('ssp_ocorrecencias_registradas.csv',mode='a', header=True ,index=False, encoding='utf-8')
-                i+=1
+
                 yield scrapy.FormRequest(self.site_url, headers=header, formdata = data, callback=self.parse_months,  dont_filter=False)
 
     def parse_months(self,response):
@@ -88,6 +114,8 @@ class IcmsSpider(scrapy.Spider):
 
         final_cols = ['municipio','regiao','delegacia','ano'] + cols_name + ['data_de_captura']
         df = df[final_cols]        
-        df.to_csv('ssp_ocorrecencias_registradas.csv',mode='a', header=True ,index=False, encoding='utf-8')
+        # df.to_csv('ssp_ocorrecencias_registradas.csv',mode='a', header=True ,index=False, encoding='utf-8')
 
+        path = '../../../dados/'
+        append_to_csv(df,'ssp_ocorrecencias_registradas',path)
         # time.sleep(20)
